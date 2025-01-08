541cb69bb3974f15221c5bca55a9e47881326889
daylon <daylonfellipe3001@outlook.com>
2025-01-08 13:03:51 +0000

    WIP - Update project structure and add ProductController

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Daylon.Padaria.Estoque.Communication;
+using Daylon.Padaria.Estoque.Communication.Requests;
+using Daylon.Padaria.Estoque.Communication.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Daylon.Padaria.Estoque.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisteredProductJson), StatusCodes.Status201Created)]
+        public IActionResult Register(RequestRegisterProductJson request)
+        {
+            return Created();
+        }
+    }
+}
